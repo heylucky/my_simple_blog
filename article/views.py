e2631ff4b76d7dc85fd7135624303ff009a55afe
heylucky <linyue219@126.com>
2017-02-18 12:05:31 +0800

    增加了分页功能，使用django内置的django.core.paginator import Page...

--- article/views.py
+++ article/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import Http404
+from django.core.paginator import PageNotAnInteger,Paginator,EmptyPage
 
 
 from models import Article
@@ -9,7 +10,19 @@ from models import Article
 
 class ArticleView(View):
     def get(self,request):
-        post_list = Article.objects.all()
+        posts = Article.objects.all()
+        paginator = Paginator(posts,3)
+        page = request.GET.get('page')      # 从 Http 请求中获取用户请求的页码号
+
+        try:
+            post_list = paginator.page(page)  # 根据页码号获取第几页的数据
+        except PageNotAnInteger:
+            # 异常处理，如果用户传递的page值不是整数，则把第一页的值返回给他
+            post_list = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            post_list = paginator.page(paginator.num_pages)      # paginator.num_pages :页面总数
+
         return render(request,'home.html',{'post_list': post_list})
 
     def post(self,request):
